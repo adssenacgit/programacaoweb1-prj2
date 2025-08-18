af0735eec2655dbb22bfb9f2371e9d9392ad4f3f
adssenacgit <ads@senacrio.com.br>
2025-08-18 20:51:49 -0300

    inclusão classe java usuario

--- src/main/java/com/senac/projeto2/entity/Usuario.java
+++ src/main/java/com/senac/projeto2/entity/Usuario.java
@@ -1,4 +1,54 @@
 package com.senac.projeto2.entity;
 
+import jakarta.persistence.Entity;
+
+import java.time.LocalDate;
+
+@Entity
 public class Usuario {
+    private int id;
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private int status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
