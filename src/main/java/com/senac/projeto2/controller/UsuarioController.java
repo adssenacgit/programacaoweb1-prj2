a1ddcdf3f69520fa5fc0f7c5f8f32946fe4f9656
adssenacgit <ads@senacrio.com.br>
2025-08-18 21:47:59 -0300

    inclusão dos metodos no controller listar todos e listar por id

--- src/main/java/com/senac/projeto2/controller/UsuarioController.java
+++ src/main/java/com/senac/projeto2/controller/UsuarioController.java
@@ -1,24 +1,41 @@
 package com.senac.projeto2.controller;
 
+import com.senac.projeto2.entity.Usuario;
+import com.senac.projeto2.service.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/usuario")
 @Tag(name="Usuario", description="API para gerenciamento dos usuarios do sistema")
 public class UsuarioController {
 
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
     @GetMapping("/listar")
     @Operation(summary = "Listar usuarios do sistema")
-    public String listar(){
-        return "Listando com sucesso";
+    public ResponseEntity<List<Usuario>> listar(){
+        return ResponseEntity.ok(usuarioService.listarUsuarios());
     }
 
     @GetMapping("/listarPorIdUsuario/{idUsuario}")
     @Operation(summary = "Listar usuarios do sistema pelo id do usuário")
-    public String listarPorIdUsuario(@PathVariable("idUsuario") Integer idUsuario){
-        return "Listando um usuario por id "+ idUsuario +" com sucesso";
+    public ResponseEntity<Usuario> listarPorIdUsuario(@PathVariable("idUsuario") Integer idUsuario){
+        Usuario usuario = usuarioService.listarUsuarioPorId(idUsuario);
+        if (usuario == null) {
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.ok(usuario);
+        }
+
     }
 
     @PostMapping("/criar")
